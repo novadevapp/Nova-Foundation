8ef7d2b9f28cdd3b5d03ab6f6cd70e048c3646eb
dubhcait <dubhcait@live.ie>
2019-08-06 17:42:01 +0100

    reset app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
-import Header from "./Component/CommonComponent/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header isLogged="true"/>
     </div>
   );
 }
