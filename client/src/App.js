b2aef02e30f47f13d4af3dc2637f880981e1cdc5
dalmano <dalmano@outlook.com>
2019-08-06 15:24:27 +0100

    Register button Relates #18

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import RegisterButton from "./Component/CommonComponent/Button/index.js";
 
 function App() {
-  return (
-    <div className="App">
-      <RegisterButton />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
