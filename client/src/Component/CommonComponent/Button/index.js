02743714ff4c49b28497343ab624c0aa5eed5bd3
dalmano <dalmano@outlook.com>
2019-08-07 16:38:54 +0100

    Refactor index.js. To use classes, add className and text for each button Relates #18

--- client/src/Component/CommonComponent/Button/index.js
+++ client/src/Component/CommonComponent/Button/index.js
@@ -1,26 +1,12 @@
 import React from "react";
 import "./style.css";
 
-export const RegisterButton = () => {
+export const button = ({ className, onClick, name }) => {
   return (
     <div>
-      <button className="register__button">Register</button>
-    </div>
-  );
-};
-
-export const ProceedButton = () => {
-  return (
-    <div>
-      <button className="proceed__button">Proceed</button>
-    </div>
-  );
-};
-
-export const SmallBackButton = () => {
-  return (
-    <div>
-      <button className="small-back__button">Back</button>
+      <button className={className} onClick={onClick}>
+        {name}
+      </button>
     </div>
   );
 };
