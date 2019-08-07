51c74f887771483988251a1d771cc806841fd5e0
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-07 16:35:21 +0300

    add menu for not login user

--- client/src/Component/CommonComponent/Menu/index.js
+++ client/src/Component/CommonComponent/Menu/index.js
@@ -9,17 +9,42 @@ class Menu extends Component {
     this.props.history.push(value);
   };
   render() {
-    return (
-      <div className="menu">
-        <li onClick={this.handleClick("/home")} className="menu__item">NOVA HOME</li>
-        <li onClick={this.handleClick("/information")} className="menu__item">NOVA INFORMATION</li>
-        <li onClick={this.handleClick("/stories")} className="menu__item">NOVA STORIES</li>
-        <li onClick={this.handleClick("/personal-space")} className="menu__item">
-          MY PERSONAL CORNAER
-        </li>
-        <li onClick={this.handleClick("/logout")} className="menu__item">LOGOUT</li>
-      </div>
-    );
+    const { isLogged } = this.props;
+    if (isLogged)
+      return (
+        <div className="menu">
+          <li onClick={this.handleClick("/home")} className="menu__item">
+            NOVA HOME
+          </li>
+          <li onClick={this.handleClick("/information")} className="menu__item">
+            NOVA INFORMATION
+          </li>
+          <li onClick={this.handleClick("/stories")} className="menu__item">
+            NOVA STORIES
+          </li>
+          <li
+            onClick={this.handleClick("/personal-space")}
+            className="menu__item"
+          >
+            MY PERSONAL CORNAER
+          </li>
+          <li onClick={this.handleClick("/logout")} className="menu__item">
+            LOGOUT
+          </li>
+        </div>
+      );
+
+      return <div className="menu">
+      <li onClick={this.handleClick("/")} className="menu__item">
+        NOVA HOME
+      </li>
+      <li onClick={this.handleClick("/login")} className="menu__item">
+        LOGIN
+      </li>
+      <li onClick={this.handleClick("/register")} className="menu__item">
+        SIGN UP
+      </li>
+    </div>
   }
 }
 
