9b9692de40f72e209ece1834f3bd353e4c716af5
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-08 10:40:40 +0300

    add the landing page route and not found route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import {Land} from './Component/Pages';
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path = '/' component = {Land} exact />
+          <Route render = {() => <div>Not Found</div>} />
+        </Switch>
+      </Router>
     </div>
   );
 }
