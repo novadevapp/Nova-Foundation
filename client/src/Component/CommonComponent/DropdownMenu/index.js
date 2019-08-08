8d3d523c60ed3779fd78f35a91f3f640ebbe24c4
dubhcait <dubhcait@live.ie>
2019-08-08 10:46:59 +0100

    use hooks for holding selection

--- client/src/Component/CommonComponent/DropdownMenu/index.js
+++ client/src/Component/CommonComponent/DropdownMenu/index.js
@@ -3,15 +3,23 @@ import React from 'react';
 import "./style.css";
 
 export default () => {
+
+    const [selection,setSelection] = React.useState("information")
+
+    React.useEffect(() => {
+     console.log(selection) ; 
+      }, [selection]);
+
     const redirectOnSelect = (e) => {
-        console.log(`/${e.target.value}`);
+        setSelection(e.target.value);
       }
 
     return (
-
+        <div className="select-container">
         <select onChange={redirectOnSelect}>
   <option value="information">Information & Suggestions</option>
   <option value="stories">Stories</option>
   <option value="suggestions">What people said</option>
 </select>
+</div>
     )}
