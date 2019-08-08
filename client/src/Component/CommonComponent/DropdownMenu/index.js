bbb3b8fbc93a4e315cdf6f0293f975c974c47980
dubhcait <dubhcait@live.ie>
2019-08-08 13:02:02 +0100

    import pages into dropdown component

--- client/src/Component/CommonComponent/DropdownMenu/index.js
+++ client/src/Component/CommonComponent/DropdownMenu/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import Footer from '../Footer'
+import React from "react";
 import "./style.css";
+import Information from "../../Pages/Information";
+import Stories from "../../Pages/Stories";
+import Suggestions from "../../Pages/Suggestion";
 
 export default () => {
+  const [selection, setSelection] = React.useState("information");
 
-    const [selection,setSelection] = React.useState("footer")
+  const redirectOnSelect = e => {
+    setSelection(e.target.value);
+  };
 
-
-    const redirectOnSelect = (e) => {
-        setSelection(e.target.value);
-      }
-
-    return (
-      <>
-        <div className="select-container">
+  return (
+    <>
+      <div className="select-container">
         <select onChange={redirectOnSelect}>
-  <option value="information">Information & Suggestions</option>
-  <option value="stories">Stories</option>
-  <option value="suggestions">What people said</option>
-</select>
-</div>
- {selection === "footer" ? <Footer/> : null}
-  </>  )}
+          <option value="information">Information & Suggestions</option>
+          <option value="stories">Stories</option>
+          <option value="suggestions">What people said</option>
+        </select>
+      </div>
+      {selection === "information" ? <Information /> : null}
+      {selection === "stories" ? <Stories /> : null}
+      {selection === "suggestions" ? <Suggestions /> : null}
+    </>
+  );
+};
