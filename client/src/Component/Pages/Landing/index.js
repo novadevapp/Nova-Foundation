f677cb9dc13b8764694939d4f49bed703cfd1556
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-08 16:50:03 +0300

    add local storage

--- client/src/Component/Pages/Landing/index.js
+++ client/src/Component/Pages/Landing/index.js
@@ -14,41 +14,45 @@ export default class Land extends Component {
   notificationContent = React.createRef();
 
   componentDidMount() {
-    this.notificationDOMRef.current.addNotification({
-      content: (
-        <div className="content" ref={this.notificationContent}>
-          <h2>
-            <span className="content__icon">
-              <Victor />
-            </span>{" "}
-            Disclaimer
-          </h2>
-          <p className="content__paragraph">
-            This prototype has been design to support parents who have suffered
-            babby loss any stage of pregnancy and loss of a child up to 12
-            months old. This content may be triggering for some, if it is
-            triggering for you please do not continue. We would appreciate any
-            feedback you can give us.
-          </p>
-          <Button
-            className="proceed__button"
-            name="Proceed"
-            onClick={this.handleClick}
-          />
-        </div>
-      ),
-      insert: "top",
-      container: "top-center",
-      dismissable: { click: true }
-    });
+    const visited = localStorage.getItem("visit");
+    if (visited !== "true") {
+      this.notificationDOMRef.current.addNotification({
+        content: (
+          <div className="content" ref={this.notificationContent}>
+            <h2>
+              <span className="content__icon">
+                <Victor />
+              </span>{" "}
+              Disclaimer
+            </h2>
+            <p className="content__paragraph">
+              This prototype has been design to support parents who have
+              suffered babby loss any stage of pregnancy and loss of a child up
+              to 12 months old. This content may be triggering for some, if it
+              is triggering for you please do not continue. We would appreciate
+              any feedback you can give us.
+            </p>
+            <Button
+              className="proceed__button"
+              name="Proceed"
+              onClick={this.handleClick}
+            />
+          </div>
+        ),
+        insert: "top",
+        container: "top-center",
+        dismissable: { click: true }
+      });
+      localStorage.setItem("visit", "true");
+    } 
   }
   handleClick = () => {
     console.log(this.notificationDOMRef);
     this.notificationContent.current.classList.add("hidden");
   };
-  handleLinks = (value) => (e) => {
-    this.props.history.push(value)
-  }
+  handleLinks = value => e => {
+    this.props.history.push(value);
+  };
 
   render() {
     return (
@@ -59,9 +63,21 @@ export default class Land extends Component {
             <h3 className="land__header"> YOU'RE NOT ALONE</h3>
             <img src={background} alt="landing image" />
             <div className="land__links">
-              <Button className="register__button" name="About Us" onClick={this.handleLinks('/about-us')}/>
-              <Button className="register__button" name="Register" onClick={this.handleLinks('register')}/>
-              <Button className="register__button" name="Login" onClick={this.handleLinks('/login')}/>
+              <Button
+                className="register__button"
+                name="About Us"
+                onClick={this.handleLinks("/about-us")}
+              />
+              <Button
+                className="register__button"
+                name="Register"
+                onClick={this.handleLinks("register")}
+              />
+              <Button
+                className="register__button"
+                name="Login"
+                onClick={this.handleLinks("/login")}
+              />
             </div>
           </div>
           <ReactNotification ref={this.notificationDOMRef} />
