bdfc733015e9d408d63799ad82d6f47faa13b617
dubhcait <dubhcait@live.ie>
2019-08-08 18:07:21 +0100

    add pages/index.js context, start routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,69 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import {
+  Landing,
+  AboutUs,
+  AddPic,
+  AddPoem,
+  AddStory,
+  Exercises,
+  Home,
+  Login,
+  PersonalSpace,
+  PicturesPage,
+  Poems,
+  Signup,
+  SingleExercises,
+  SinglePoem,
+  SingleStory,
+  Status,
+  Stories,
+  Suggestion
+} from "./Component/Pages";
+
 import "./App.css";
 
+//if logged in
+const navLinksForUsers = [
+  { path: "/", component: Home },
+  { path: "/about-us", component: AboutUs }
+];
+
+//if not logged in
+const navLinksForVisitors = [
+  { path: "/", component: Landing },
+  { path: "/login", component: Login },
+  { path: "/signup", component: Signup }
+];
 
 function App() {
+  const loggedIn = true;
   return (
     <div className="App">
-   
+      <Router>
+        <Switch>
+          {loggedIn === true
+            ? navLinksForUsers.map((route, index) => (
+                <Route
+                  exact
+                  path={route.path}
+                  key={index}
+                  component={route.component}
+                />
+              ))
+            : navLinksForVisitors.map((route, index) => (
+                <Route
+                  exact
+                  path={route.path}
+                  key={index}
+                  component={route.component}
+                />
+              ))}
+
+          <Route render={() => <div>Not Found</div>} />
+        </Switch>
+      </Router>
     </div>
   );
 }
