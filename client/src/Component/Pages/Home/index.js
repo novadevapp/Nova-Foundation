4d2a29e07d0ad433e7d70e337231ea11c82da96b
Israa <israa96sulaiman@gmail.com>
2019-08-08 16:41:30 +0300

    remove h3 from header and edit some css Relates #28

--- client/src/Component/Pages/Home/index.js
+++ client/src/Component/Pages/Home/index.js
@@ -12,7 +12,7 @@ export default function (props) {
 
   return (
     <>
-      <Header style={{ position: 'relative' }} isLogged='false' className='header' />
+      <Header isLogged='false' className='header'  />
       <main className='home-page'>
         <section className='home-page__section'>
           <h2 className='home-page__title'>Discover our resources. We hope they make you feel better!</h2>
