01a3847e0d8e27d1c99f10293e4beb5e68b50c19
Kate <39469813+dubhcait@users.noreply.github.com>
2019-08-08 12:25:11 +0100

    use short hand for React.Fragment

--- client/src/Component/CommonComponent/Textarea/index.js
+++ client/src/Component/CommonComponent/Textarea/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 export default ({ id, label, placeholder, action }) => {
   return (
-    <React.Fragment>
+    <>
       <label htmlFor={id} > {label}  </label>
         <textarea id={id} name={id} placeholder={placeholder} onBlur={action}/>
 
-    </React.Fragment>
+    </>
   );
 };
