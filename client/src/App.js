56790d99359838d7ede20eb27df1925ef88c54b8
dubhcait <dubhcait@live.ie>
2019-08-08 18:18:24 +0100

    comment out unwritten pages

--- client/src/App.js
+++ client/src/App.js
@@ -26,15 +26,28 @@ import "./App.css";
 
 //if logged in
 const navLinksForUsers = [
-  { path: "/", component: Home },
-  { path: "/about-us", component: AboutUs }
+  { path: "/", component: Home }
+  // { path: "/about-us", component: AboutUs },
+  // { path: "/status", component: Status },
+  // { path: "/exercises", component: Exercises },
+  // { path: "/personal-space", component: PersonalSpace },
+  //  { path: "/add-pic", component: AddPic },
+  // { path: "/add-poem", component: AddPoem },
+  // { path: "/add-story", component: AddStory },
+  // { path: "/pictures", component: PicturesPage },
+  // { path: "/poems", component: Poems },
+  // { path: "/stories", component: Stories },
+  // { path: "/suggestion", component: Suggestion },
+  // { path: "/single-story", component: SingleStory },
+  // { path: "/single-poem", component: SinglePoem },
+  // { path: "/single-exercise", component: SingleExercises }
 ];
 
 //if not logged in
 const navLinksForVisitors = [
-  { path: "/", component: Landing },
-  { path: "/login", component: Login },
-  { path: "/signup", component: Signup }
+  { path: "/", component: Landing }
+  // { path: "/login", component: Login },
+  // { path: "/signup", component: Signup }
 ];
 
 function App() {
