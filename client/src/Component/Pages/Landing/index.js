772ee7bc8b9e7fd6167f0b533eeb0cd2e4c814a3
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-08 16:44:43 +0300

    add click function to buttons

--- client/src/Component/Pages/Landing/index.js
+++ client/src/Component/Pages/Landing/index.js
@@ -4,7 +4,7 @@ import ReactNotification from "react-notifications-component";
 import Header from "../../CommonComponent/Header";
 import Footer from "../../CommonComponent/Footer";
 import background from "../../../assets/background.png";
-import Button  from "../../CommonComponent/Button";
+import Button from "../../CommonComponent/Button";
 import Victor from "./victor";
 
 import "./style.css";
@@ -15,31 +15,39 @@ export default class Land extends Component {
 
   componentDidMount() {
     this.notificationDOMRef.current.addNotification({
-      content : (<div className="content" ref={this.notificationContent}>
-      <h2>
-        <Victor /> Disclaimer
-      </h2>
-      <p className="content__paragraph">
-        This prototype has been design to support parents who have suffered
-        babby loss any stage of pregnancy and loss of a child up to 12
-        months old. This content may be triggering for some, if it is
-        triggering for you please do not continue. We would appreciate any
-        feedback you can give us.
-      </p>
-      <Button
-        className="proceed__button"
-        name="Proceed"
-        onClick={this.handleClick}
-      />
-    </div>),
+      content: (
+        <div className="content" ref={this.notificationContent}>
+          <h2>
+            <span className="content__icon">
+              <Victor />
+            </span>{" "}
+            Disclaimer
+          </h2>
+          <p className="content__paragraph">
+            This prototype has been design to support parents who have suffered
+            babby loss any stage of pregnancy and loss of a child up to 12
+            months old. This content may be triggering for some, if it is
+            triggering for you please do not continue. We would appreciate any
+            feedback you can give us.
+          </p>
+          <Button
+            className="proceed__button"
+            name="Proceed"
+            onClick={this.handleClick}
+          />
+        </div>
+      ),
       insert: "top",
       container: "top-center",
       dismissable: { click: true }
     });
   }
   handleClick = () => {
-    console.log(this.notificationDOMRef)
-    this.notificationContent.current.classList.add('hidden');
+    console.log(this.notificationDOMRef);
+    this.notificationContent.current.classList.add("hidden");
+  };
+  handleLinks = (value) => (e) => {
+    this.props.history.push(value)
   }
 
   render() {
@@ -50,9 +58,11 @@ export default class Land extends Component {
           <div className="land__home">
             <h3 className="land__header"> YOU'RE NOT ALONE</h3>
             <img src={background} alt="landing image" />
-            <Button className="register__button" name="About Us" />
-            <Button className="register__button" name="Register" />
-            <Button className="register__button" name="Login" />
+            <div className="land__links">
+              <Button className="register__button" name="About Us" onClick={this.handleLinks('/about-us')}/>
+              <Button className="register__button" name="Register" onClick={this.handleLinks('register')}/>
+              <Button className="register__button" name="Login" onClick={this.handleLinks('/login')}/>
+            </div>
           </div>
           <ReactNotification ref={this.notificationDOMRef} />
         </main>
