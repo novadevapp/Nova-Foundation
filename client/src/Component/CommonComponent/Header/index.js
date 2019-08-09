110418876f63d899bdfea4ddf1bdddc9a3f80833
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-09 13:06:12 +0300

    add toggle component to the header

--- client/src/Component/CommonComponent/Header/index.js
+++ client/src/Component/CommonComponent/Header/index.js
@@ -3,13 +3,22 @@ import "./style.css";
 import Star from "./star";
 import Cloud from "./cloud";
 import Logo from "./logo";
+import Toggle from "./toggle";
 import Triangle from "./triangle";
+import Menu from "../Menu";
 
 const Header = ({ isLogged, className }) => {
-if(className == null) {
+  const menuRef = React.createRef();
+
+  function handleClick() {
+    menuRef.current.menuRef.current.classList.toggle("menu--show");
+  }
+  if (className == null) {
     return (
+      <>
         <div className={`header minimal`}>
           <Logo />
+          <Toggle onClick={handleClick} />
           <Star className="star-two" />
           <Star className="star-three" />
           <Star className="star-nine" />
@@ -17,36 +26,40 @@ if(className == null) {
           <Star className="star-eleven" />
           <Star className="star-twelve" />
         </div>
-      );
-}
+        <Menu isLogged={isLogged} ref={menuRef} />
+      </>
+    );
+  }
 
   return (
-    <div className={`header`}>
-      <Logo />
-      <Star className="star-one" />
-      <Star className="star-two" />
-      <Star className="star-three" />
-      <Star className="star-four" />
-      <Star className="star-five" />
-      <Star className="star-six" />
-      <Star className="star-seven" />
-      <Star className="star-eight" />
-      {className === "register" ? (
-        <React.Fragment>
-          <h1> Create your Account</h1>{" "}
-          <h3>
-            Complete your account to get access to our support
+    <>
+      <div className={`header`}>
+        <Logo />
+        <Toggle onClick={handleClick} />
+        <Star className="star-one" />
+        <Star className="star-two" />
+        <Star className="star-three" />
+        <Star className="star-four" />
+        <Star className="star-five" />
+        <Star className="star-six" />
+        <Star className="star-seven" />
+        <Star className="star-eight" />
+        {className === "register" ? (
+          <React.Fragment>
+            <h1> Create your Account</h1>{" "}
+            <h3>Complete your account to get access to our support</h3>
+          </React.Fragment>
+        ) : null}
+        {className === "register" ? <Cloud /> : null}
+        {className === "home" ? (
+          <h3 className="home-h3">
+            Discover our resources. We hope they make you feel better!
           </h3>
-        </React.Fragment>
-      ) : null}
-      {className === "register" ? <Cloud /> : null}
-      {className === "home" ? (
-        <h3 className="home-h3">
-          Discover our resources. We hope they make you feel better!
-        </h3>
-      ) : null}
-      {className === "triangle" ? <Triangle /> : null}
-    </div>
+        ) : null}
+        {className === "triangle" ? <Triangle /> : null}
+      </div>
+      <Menu isLogged={isLogged} ref={menuRef} />
+    </>
   );
 };
 
