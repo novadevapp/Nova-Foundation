c9291f1e23846959cea9ab2b9d5a864b6c1c823e
Israa <israa96sulaiman@gmail.com>
2019-08-09 11:39:45 +0300

    create personal space page & its css Relates #29

---
+++ client/src/Component/Pages/PersonalSpace/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Header from '../../CommonComponent/Header';
+import HomeIcon from '../../CommonComponent/ServiceCard/homeIcon';
+import Button from '../../CommonComponent/Button';
+import './style.css'
+
+export default function (props) {
+
+  function handleRedirection(path) {
+    props.history.push(path);
+  }
+
+  return (
+    <>
+      <Header />
+      <main className='personal-space'>
+        <section className='personal-space__section'>
+          <div className='personal-space__header'>
+            <HomeIcon />
+            <h3>Personal Space</h3>
+          </div>
+          <div className='personal-space__cards'>
+            <div className='personal-space__row'>
+              <Button
+                name='Pictures'
+                className='personal-space__button'
+                onClick={() => handleRedirection('/pictures')}
+              />
+              <Button
+                name='Poems'
+                className='personal-space__button'
+                onClick={() => handleRedirection('/poems')}
+              />
+            </div>
+            <div className='personal-space__row'>
+              <Button
+                name='Exercises'
+                className='personal-space__button'
+                onClick={() => handleRedirection('/exercises')}
+              />
+              <Button
+                name='Journal'
+                className='personal-space__button'
+                onClick={() => handleRedirection('/journal')}
+              />
+            </div>
+          </div>
+          <Button
+            name='Back'
+            className='large-skip__button'
+            onClick={() => props.history.goBack()}
+          />
+        </section>
+      </main>
+    </>
+
+  )
+}
