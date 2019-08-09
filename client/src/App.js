f48cff6bfcbcb9bf62c9240a6869acb19df73d28
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-09 23:23:34 +0300

    remove header from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from './Component/CommonComponent/Header';
 import "./App.css";
 
 
 function App() {
   return (
     <div className="App">
-      <Header isLogged = {true}/>
+   
     </div>
   );
 }
