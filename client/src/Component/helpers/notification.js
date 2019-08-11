c4513562874d6642365c04a9fbbc75ca5dfe0a08
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-11 09:30:33 +0300

    create notification helper

---
+++ client/src/Component/helpers/notification.js
@@ -0,0 +1,12 @@
+export default (ref, type, message) => {
+    ref.current.addNotification({
+        message,
+        type,
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animated", "fadeIn"],
+        animationOut: ["animated", "fadeOut"],
+        dismiss: { duration: 2000 },
+        dismissable: { click: true }
+      });
+};
