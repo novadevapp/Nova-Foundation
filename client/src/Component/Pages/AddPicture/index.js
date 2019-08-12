dd88c358bc2134b822206553b638e5dcd2213bad
dubhcait <dubhcait@live.ie>
2019-08-12 12:26:44 +0100

    add error handling notification

--- client/src/Component/Pages/AddPicture/index.js
+++ client/src/Component/Pages/AddPicture/index.js
@@ -8,31 +8,59 @@ import Input from "../../CommonComponent/Input";
 import "./style.css";
 
 export default function(props) {
-  function handleRedirection(path) {
-    props.history.push(path);
-  }
-
   const [title, setTitle] = React.useState("");
   const [file, setFile] = React.useState("");
+  const [error, SetError] = React.useState(false);
+  const [typeError, SetTypeError] = React.useState(false);
+  const [fetchError, SetfetchError] = React.useState(false);
+
+  const acceptedTypes = ["jpeg", "jpg", "gif", "png"];
+  let type = file.split(".")[1];
+
+  const message = {
+    default: "Please add a title and upload a file",
+    tyrebased: "Unfortunately we do not support file type: ",
+    fetch: "Sorry something went wrong, Please try again"
+  };
 
   const handleSubmit = e => {
     const Button = document.querySelector(".large-save__button");
-    Button.textContent = "Loading...";
     e.preventDefault();
-    fetch("/send-pic", {
-      method: "post",
-      body: JSON.stringify({
-        title: `${title}`,
-        file: `${file}`
+    if ((title === "") | (file === "")) {
+      SetError(true);
+    } else if (acceptedTypes.indexOf(type) === -1) {
+      SetTypeError(true);
+    } else {
+      SetError(false);
+      SetTypeError(false);
+
+      Button.textContent = "Loading...";
+
+      fetch("/api/v1/send-pic", {
+        method: "post",
+        body: JSON.stringify({
+          title: `${title}`,
+          file: `${file}`
+        })
       })
-    })
-      .then(response => response.json())
-      .then(res => {
-        if (res.status !== 200) {
-          console.log(res);
-        }
-        handleRedirection("/pictures");
-      });
+        .then(response => response.json())
+        .then(({ error, data }) => {
+          if (error) {
+            {
+              Button.textContent = "Save";
+            }
+            SetfetchError(true);
+          } else {
+            this.props.history.push("/pictures");
+          }
+        })
+        .catch(() => {
+          {
+            Button.textContent = "Save";
+          }
+          SetfetchError(true);
+        });
+    }
   };
 
   return (
@@ -47,7 +75,9 @@ export default function(props) {
               placeholder="Add a title"
               label="Title"
               type="text"
-              action={e => setTitle(e.target.value)}
+              action={e => {
+                setTitle(e.target.value);
+              }}
             />
             <Input
               id="new-pic__file"
@@ -55,6 +85,15 @@ export default function(props) {
               type="file"
               action={e => setFile(e.target.value)}
             />
+            {error && <p className="add-pic__error">{message.default}</p>}
+            {typeError && (
+              <p className="add-pic__error">
+                {message.tyrebased}
+                {type}
+              </p>
+            )}
+
+            {fetchError && <p className="add-pic__error">{message.fetch}</p>}
             <div className="add-pic-page__form_buttons">
               <a href="/pictures" className="large-skip__button">
                 Back
