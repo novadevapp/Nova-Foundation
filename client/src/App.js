40752ac82b59b32d591dd9c846b80874730c362f
Dylan A <30301023+Dalmano@users.noreply.github.com>
2019-08-12 13:33:12 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import Pictures from "../../client/src/Component/Pages/Pictures/index";
 
 function App() {
   return (
     <div className="App">
-      <Pictures />
+
     </div>
   );
 }
