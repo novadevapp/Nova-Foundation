5c9637506abd4453885c4c4518e65e6153a3a6ab
Israa <israa96sulaiman@gmail.com>
2019-08-14 01:59:51 +0300

    create initial models

---
+++ src/database/models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+  },
+  babyName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+    trim: true,
+  }
+});
+
+module.exports = mongoose.model('users', UserSchema);
