22a27dd4900bd3e854647f89dbc20a1ad0b910a2
Israa <israa96sulaiman@gmail.com>
2019-08-14 02:23:10 +0300

    fix bug in findOne func

--- src/database/config/buildTestDB/setPoems.js
+++ src/database/config/buildTestDB/setPoems.js
@@ -1,11 +1,10 @@
 const Poem = require('../../models/poems');
 const User = require('../../models/users');
-const connect = require('../../config/connection');
 
 module.exports = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const user = await User.findOne({ name: 'Sarah' });
+      const user = await User.findOne({ username: 'Sarah' });
       const PoemOne = {
         title: "My First Child",
         content: "Since the day I you were in my tummy, I knew I'd be a great mommy, I'd love to feel every kick you gave, How I wish I'd see you wave...",
