f67dbb6f699e0ef43f4aeffbdc770901eef49593
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 21:26:11 +0300

    handle the error

--- src/controller/picture/getPictures.js
+++ src/controller/picture/getPictures.js
@@ -2,9 +2,11 @@ const admin = require("../../database/firebaseStorage/config");
 const pictuers = async (req, res) => {
   const bucket = admin.storage().bucket();
 
-  // the id of user
+  // the id of user we must take it from the cookie
+  let { id } = req.cookies;
+  id = 1; // disable this line when compelete the app (for testing)
   const options = {
-    prefix: "1/"
+    prefix: `${id}/`
   };
 
   const optionSignUrl = {
@@ -12,14 +14,17 @@ const pictuers = async (req, res) => {
     action: "read",
     expires: Date.now() + 15 * 60 * 1000 // 15 minutes
   };
-
-  const [files] = await bucket.getFiles(options);
-  const imagesName = files.filter(file => file.name.split("1/")[1]);
-  const images = [];
-  imagesName.forEach(async (file, index) => {
-    const [url] = await bucket.file(file.name).getSignedUrl(optionSignUrl);
-    images.push(url);
-    if (images.length === imagesName.length) res.send({ images });
-  });
+  try {
+    const [files] = await bucket.getFiles(options);
+    const imagesName = files.filter(file => file.name.split("1/")[1]);
+    const images = [];
+    imagesName.forEach(async (file) => {
+      const [url] = await bucket.file(file.name).getSignedUrl(optionSignUrl);
+      images.push(url);
+      if (images.length === imagesName.length) res.send({ images });
+    });
+  } catch (error) {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
 };
 module.exports = pictuers;
