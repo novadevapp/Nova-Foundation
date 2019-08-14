3b95e7451be811e44e675bc3c7c1f179f2fac950
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 17:46:21 +0300

    modify the app by add the controller

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const path = require("path");
 const compresion = require("compression");
 const helmet = require("helmet");
-const admin = require("./firebase/index");
 const cookie = require("cookie-parser");
-const formidable = require("formidable");
+const controller = require('./controller');
 
 const app = express();
 
@@ -27,48 +26,7 @@ app.use(function(req, res, next) {
 
 app.use(middleware);
 
-const postPiC = async (req, path, res) => {
-  // [START storage_upload_file]
-  // Imports the Google Cloud client library
-  const { Storage } = require("@google-cloud/storage");
-
-  // Creates a client
-  const storage = new Storage();
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  const bucketName = "nova-foundation.appspot.com";
-  const filename = path;
-
-  // Uploads a local file to the bucket
-  await storage.bucket(bucketName).upload(filename, {
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
-    gzip: true,
-    // By setting the option `destination`, you can change the name of the
-    // object you are uploading to a bucket.
-    metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
-      cacheControl: "public, max-age=31536000"
-    }
-  });
-
-  console.log(`${filename} uploaded to ${bucketName}.`);
-  // [END storage_upload_file]
-};
-
-app.get("/express_backend", (req, res) => {
-  res.send({ express: " YOUR BACKEND IS CONNECTED" });
-});
-
-app.post("/api/v1/send-pic", (req, res) => {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
-    postPiC(req, files.file.path, res);
-  });
-});
+app.use('/api/v1', controller);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
