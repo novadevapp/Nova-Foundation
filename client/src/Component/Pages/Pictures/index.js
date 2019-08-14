74f5f66a2eefc7a463990ebd5dec791c9c3d30fd
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 20:51:45 +0300

    add loading to the page

--- client/src/Component/Pages/Pictures/index.js
+++ client/src/Component/Pages/Pictures/index.js
@@ -4,28 +4,34 @@ import Header from "../../CommonComponent/Header/index";
 import Gallery from "./gallery";
 import Button from "../../CommonComponent/Button/index";
 import Footer from "../../CommonComponent/Footer";
+import Loading from "../../CommonComponent/Loading";
 
 export default class Pictures extends Component {
   state = {
-    imageURL: []
+    imageURL: [],
+    loading: true
   };
   componentDidMount() {
     fetch("/api/v1/pictures")
       .then(res => res.json())
-      .then(({ images }) => {
-        this.setState({ imageURL: images });
+      .then(({ images, error }) => {
+        if (error) console.log(error);
+        else this.setState({ imageURL: images, loading: false });
       })
       .catch(err => {
         console.log(err);
       });
   }
   render() {
-    const { imageURL } = this.state;
+    const { imageURL, loading } = this.state;
     return (
       <div>
         <Header />
-
-        <Gallery className="img__single" imgUrl={imageURL} />
+        <section className="collage-container">
+          <h1 className="all-pictures__title"> All Pictures </h1>
+          {loading && <Loading />}
+          <Gallery className="img__single" imgUrl={imageURL} />
+        </section>
         <div className="pic__buttons">
           <Button className="large-back__button" name="Back" />
           <Button className="register__button" name="More Pictures" />
