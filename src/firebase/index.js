76591f498b0912af7206f14d9d1e608dd10c545d
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 17:46:42 +0300

    delete folder of firebase

--- src/firebase/index.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-require("dotenv").config();
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.credentials)),
-  storageBucket: process.env.STORAGEBUCKET
-});
-
-module.exports = admin;
