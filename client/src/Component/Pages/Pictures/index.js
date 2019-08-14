c2797e38da67c9db97835e05995082b19849b82f
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 18:45:54 +0300

    create initial view pictures

--- client/src/Component/Pages/Pictures/index.js
+++ client/src/Component/Pages/Pictures/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./style.css";
 import Header from "../../CommonComponent/Header/index";
 import Gallery from "./gallery";
@@ -7,25 +7,25 @@ import Footer from "../../CommonComponent/Footer";
 
 export default class Pictures extends Component {
   state = {
-    imageURL : []
-  }
-  componentDidMount(){
-    fetch('/api/v1/pictures')
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    imageURL: []
+  };
+  componentDidMount() {
+    fetch("/api/v1/pictures")
+      .then(res => res.json())
+      .then(({ images }) => {
+        this.setState({ imageURL: images });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
-    const {imageURL} = this.state;
+    const { imageURL } = this.state;
     return (
       <div>
         <Header />
-  
-        <Gallery className="img__single" imgUrl= {imageURL}/>
+
+        <Gallery className="img__single" imgUrl={imageURL} />
         <div className="pic__buttons">
           <Button className="large-back__button" name="Back" />
           <Button className="register__button" name="More Pictures" />
@@ -35,4 +35,3 @@ export default class Pictures extends Component {
     );
   }
 }
-
