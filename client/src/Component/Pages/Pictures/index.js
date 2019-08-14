71ade05dd9e0d83c23a73f7d9603e7121ad3bdc0
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-14 21:14:38 +0300

    add Add icon for the page

--- client/src/Component/Pages/Pictures/index.js
+++ client/src/Component/Pages/Pictures/index.js
@@ -5,6 +5,7 @@ import Gallery from "./gallery";
 import Button from "../../CommonComponent/Button/index";
 import Footer from "../../CommonComponent/Footer";
 import Loading from "../../CommonComponent/Loading";
+import AddIcon from "./AddIcon";
 
 export default class Pictures extends Component {
   state = {
@@ -22,18 +23,22 @@ export default class Pictures extends Component {
         console.log(err);
       });
   }
+  handleClick = () => {
+    this.props.history.push("/add-pic");
+  };
   render() {
     const { imageURL, loading } = this.state;
     return (
       <div>
         <Header />
+        <AddIcon onClick={this.handleClick} />
         <section className="collage-container">
           <h1 className="all-pictures__title"> All Pictures </h1>
           {loading && <Loading />}
           <Gallery className="img__single" imgUrl={imageURL} />
         </section>
         <div className="pic__buttons">
-          <Button className="large-back__button" name="Back" />
+          <Button className="large-back__button" name="Back" onClick = {() => this.props.history.goBack()}/>
           <Button className="register__button" name="More Pictures" />
         </div>
         <Footer />
