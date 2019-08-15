45b0cb9ea14b3c6dc92bac7ed08fbe287ed7232a
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-15 12:59:24 +0300

    add the delete pictures route

--- src/controller/picture/index.js
+++ src/controller/picture/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const pictures = require('./getPictures');
-const postPicture = require('./postPicture');
+const pictures = require("./getPictures");
+const postPicture = require("./postPicture");
+const deletePicture = require("./deletePicture");
 
 const route = express.Router();
 
-route.get('/pictures', pictures);
-route.post('/add-picture', postPicture);
+route
+  .route("/pictures")
+  .get(pictures)
+  .delete(deletePicture);
+  
+route.post("/add-picture", postPicture);
 
 module.exports = route;
