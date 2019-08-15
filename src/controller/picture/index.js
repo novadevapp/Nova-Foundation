7abe0c01925629054f55bbeb988216d76faa2224
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-15 08:12:30 +0300

    add route in the index of the paictures

--- src/controller/picture/index.js
+++ src/controller/picture/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const pictures = require('./getPictures');
+const postPicture = require('./postPicture');
 
 const route = express.Router();
 
 route.get('/pictures', pictures);
+route.post('/add-picture', postPicture);
 
 module.exports = route;
