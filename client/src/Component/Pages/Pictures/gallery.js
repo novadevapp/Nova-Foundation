4bee674e3c707f985c0e708f50a52b0b0b3f7974
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-15 12:35:15 +0300

    modify the src depend on the data

--- client/src/Component/Pages/Pictures/gallery.js
+++ client/src/Component/Pages/Pictures/gallery.js
@@ -1,9 +1,18 @@
 import React from "react";
+
+import DeleteIcon from './DeleteIcon';
 import "./style.css";
 
-const Gallery = ({ imgUrl }) => {
+const Gallery = ({ imgUrl, onClick}) => {
   return imgUrl.map((src, index) => (
-    <img key={index} src={src} className="img__single" alt="personal" />
+    <div className="image-content" key={index}>
+      <img src={src.url} className="img__single" alt="personal" />
+      <div className="hover">
+        <div className="hover__icon">
+        <DeleteIcon onClick = {onClick} id={index}/>
+        </div>
+      </div>
+    </div>
   ));
 };
 
