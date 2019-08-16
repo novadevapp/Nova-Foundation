d7e1b4112c26b615f404d60dbb028678ce8d9e35
dalmano <dalmano@outlook.com>
2019-08-16 18:09:07 +0100

    Create information page Relates #83

---
+++ client/src/Component/Pages/Information/content.js
@@ -0,0 +1,146 @@
+import React from "react";
+import "./style.css";
+
+const content = () => {
+  return (
+    <div className="info-container">
+      <h1 className="info-title">Nova Information</h1>
+      <section className="info-content">
+        <div>
+          <p>
+            Grief is the flipside of love, which is why it is so painful.
+            Babyloss is against the natural order of things, resulting in an
+            intense grief for the loss of your child and the loss of the life
+            you had planned. We are so sorry that this happened to you. Please
+            know that you are not alone.
+          </p>
+          <p>
+            This app was designed by babyloss parents to provide comfort to
+            other babyloss parents. It is a safe space to connect with and
+            remember your baby, whilst helping you track your own wellbeing.
+          </p>
+        </div>
+      </section>
+      <section className="grief-section">
+        <h3 className="grief-title">Grief</h3>
+        <div className="info-content">
+          <p>
+            Grief can feel like drowning in a freezing sea at midnight. It can
+            be overwhelming and frightening, but is always changing, along with
+            the feelings that go with it. Grief has no time limit.
+          </p>
+          <p>
+            You may have heard of the five stages of grief – denial, anger,
+            bargaining, depression and acceptance. These are not linear stages
+            that you will neatly go though, you may experience some but not
+            others, feel some again and again or feel them all at once. Grief is
+            more like an upwards spiral than a staircase, you will often feel
+            the same emotion, with different intensities. You will not always
+            feel like this.
+          </p>
+          <p>
+            Have you experienced any of these feelings and how do you feel right
+            now?
+          </p>
+        </div>
+      </section>
+      <section className="symptoms-section">
+        <h3 className="symptoms-title">Symptoms of Grief</h3>
+        <div className="info-content">
+          <p>
+            Body pains - Grief can manifest itself in many ways including
+            through your body. What happened to you is traumatic. You could have
+            numbness (arm numbness/aching is very common with babyloss mothers).
+            You may have migraines and headaches, or pain or tingling
+            sensations.
+          </p>
+
+          <p>Have you had any body pains and aches and if so what are they?</p>
+          <p>
+            Exhaustion - In the first six weeks it is likely you will experience
+            physical exhaustion, with emotional exhaustion lifting a bit by
+            month three. At times you may feel like you are going mad, but
+            memory loss is very common, often made worse by headaches, and,
+            although it can be frightening, is nothing to be alarmed about.
+          </p>
+          <p>
+            You may feel numb, have difficulty concentrating, have a loss of
+            appetite, poor sleep etc. At points you may feel pure despair and an
+            intense sadness. This is natural.
+          </p>
+          <p>
+            Guilt is a common reaction to babyloss. Guilt can feel like a
+            prison. Please know that nothing you said, did, ate, read caused you
+            to lose your baby. You are not to blame, it was not your fault.
+            Please let people look after you treat yourself kindly as you would
+            treat a friend.
+          </p>
+          <p>What nice things are you going to do to look after yourself?</p>
+          <p>
+            Image result for svg volcanoAnger – this could be at the hospital,
+            at the world, at other people for saying well meaning but silly
+            things, at yourself. Feeling life is meaningless, that life is
+            unjust and good people have bad things happen to them, at wanting
+            answers and having none. It is perfectly natural to feel angry about
+            what happened, it is a feeling and will pass. Talk to someone about
+            how you feel. Take time to breathe and respond to people that may
+            upset you, calmly. Journal your feelings. Get your anger out in a
+            way that feels good for you but that does not impact other people.
+          </p>
+          <p>
+            What symptoms of grief have you experienced and how do you think you
+            could let the anger out?
+          </p>
+          <p>
+            Depression – Grief and depression are two very different things.
+            They can have similar symptoms such as poor sleep, lack of appetite
+            and feeling intensely sad. The sting of grief lessens over time,
+            whereas depression does not. Depression is an illness that can be
+            treated, whereas grief subsides over time. If your feelings do not
+            change in the first few months, or if you are unsure if what you are
+            feeling is grief or depression, please go to your GP.
+          </p>
+        </div>
+      </section>
+      <section className="trauma-section">
+        <h3 className="trauma-title">Trauma </h3>
+        <div className="info-content">
+          <p>
+            Trauma – trauma is your normal bodies reaction to an abnormal event.
+            There are many crossovers between trauma and grief. The main
+            symptoms of trauma include anxiety, replaying images or moments in
+            your mind/flashbacks, being triggered, nightmares, unexplained
+            pains, feeling fearful, tinglings or numbness in your body (arm
+            pain/ache is very common for babyloss mums), feeling
+            disconnected/detached, being on edge all the time/jumpy and unable
+            to relax, having persistent haunting and unwanted thoughts, feeling
+            overwhelmed/overstimulated in public spaces, hypersensitivity to
+            sound.
+          </p>
+          <p>
+            Have you experienced any of the above symptoms and if so, what are
+            they?
+          </p>
+          <p>
+            If you feel you are traumatised you should seek professional support
+            from a trauma therapist. Trauma can be flushed out the system very
+            quickly (3-4 sessions) leaving you free to focus on grieving. If you
+            feel you have ptsd, use the app to track your feelings. If traumatic
+            symptoms persist for four weeks please go to your GP if you find the
+            wait for an appointment is too long you can go privately by googling
+            trauma therapists in your area. There is additional information
+            about trauma and different therapies available on our website.
+          </p>
+          <a
+            href=" https://www.nhs.uk/conditions/post-traumatic-stress-disorder-ptsd/treatment/"
+            target="_blank"
+          >
+            For information on treatements for PTSD click here
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default content;
---
+++ client/src/Component/Pages/Information/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+import Header from "../../CommonComponent/Header";
+import Content from "./content";
+import Footer from "../../CommonComponent/Footer";
+
+const Infomation = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Infomation;
