5392badd361d2918bcb00f60762e028d9c26103d
Kate <39469813+dubhcait@users.noreply.github.com>
2019-08-16 13:00:30 +0100

    remove server side dummy functions

--- src/controller/login/postLogin.js
+++ src/controller/login/postLogin.js
@@ -1,10 +1,1 @@
-const jwt = require("jsonwebtoken");
-const postLogin = (req, res) => {
-  const id = "trial";
-  const username = "more";
-  const signed = jwt.sign({ id, username }, process.env.SECRET);
-  res.cookie("cookienvf", signed, { maxAge: 900000 });
-  res.send({ jwt: signed });
-};
 
-module.exports = postLogin;
