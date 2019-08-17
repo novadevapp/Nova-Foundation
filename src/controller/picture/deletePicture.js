c6dca85e8e2745d6c8fdae1139f116dc4f5f7710
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-17 16:07:22 +0300

    delete unneccessary line

--- src/controller/picture/deletePicture.js
+++ src/controller/picture/deletePicture.js
@@ -2,17 +2,14 @@ const admin = require("../../database/firebaseStorage/config");
 
 const deletePicture = async (req, res) => {
   try {
-    let { id } = req.cookies;
-    // disable this line
-    id = 1;
-    console.log(req.body);
+    let { id } = req.auth;
+    
     const { fileName } = req.body.data;
     const bucket = admin.storage().bucket();
 
     await bucket.file(`${fileName}`).delete();
     res.send({ data: 'Delete File' });
   } catch (error) {
-      console.log(error);
     res.status(500).send({ error: "Internal Server Error" });
   }
 };
