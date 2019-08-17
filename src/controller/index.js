29896fb6ff53b64f4ced1d54e63c5bc189249625
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-17 16:03:08 +0300

    add authonticate middleware

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const pictures = require('./picture');
+const poems = require('./poems');
+const authonticate = require('../middleware/authenticate')
 
 const route = express.Router();
 
+
+route.use(authonticate)
 route.use(pictures);
+route.use(poems);
 
 module.exports = route;
