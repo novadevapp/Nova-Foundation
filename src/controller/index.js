2ca9e3864a0e14bc5ec243575cb570fcd207f6fb
dubhcait <dubhcait@live.ie>
2019-08-18 14:56:26 +0100

    add /status to controller

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,16 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const signup = require('./signup');
-const pictures = require('./picture');
-const login = require('./login');
-const poems = require('./poems');
+const signup = require("./signup");
+const pictures = require("./picture");
+const login = require("./login");
+const poems = require("./poems");
 const postStatus = require("./checkLogin");
+const PostThoughts = require("./thoughts");
 
-router.use('/register', signup);
+router.use("/register", signup);
 // router.use('/login', login);
-
 router.get("/login-status", postStatus);
 // router.use('/poems', poems);
+router.use("/status", PostThoughts);
 router.use(pictures);
 
 module.exports = router;
