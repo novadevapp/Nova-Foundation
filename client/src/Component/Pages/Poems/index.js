731c887c31463a35569f7eb60047e9cc3fa6c02b
shorouq <shrooqabdullahsaad@gmail.com>
2019-08-18 21:04:37 +0300

    add fetch and add icon

--- client/src/Component/Pages/Poems/index.js
+++ client/src/Component/Pages/Poems/index.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import PoemList from "./poemList";
 import Header from "../../CommonComponent/Header";
 import Footer from "../../CommonComponent/Footer";
 import Button from "../../CommonComponent/Button";
+import AddIcon from '../../CommonComponent/AddIcon';
 
-const Poems = () => {
+const Poems = (props) => {
+  const [poems, setPoems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    if (!loading) {
+      fetch('/api/v1/poems')
+      .then(res => res.json())
+      .then(({data, error}) => {
+        if(error) setError('Something Error Please Try Again')
+        else setPoems(data);
+        setLoading(true)
+      })
+      .catch(() => {
+        setError('Something Error Please Try Again')
+      })
+    }
+  });
+
+  const handleClick = () => {
+    this.props.history.push('/add-poem')
+  }
   return (
     <div>
       <Header />
-      <PoemList />
+      {error && <p className="error">{error}</p>}
+      <AddIcon onClick = {handleClick}/>
+      <PoemList poems = {poems}/>
       <div className="poem-buttons">
-        <Button className="large-back__button" name="Back" />
+        <Button className="large-back__button" name="Back" onClick = {() => this.props.history.goBack()} />
         <Button className="register__button" name="More Poems" />
       </div>
 
