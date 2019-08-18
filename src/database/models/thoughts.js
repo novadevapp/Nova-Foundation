ac727187bc82bf8bce523d5cda13c23f47b33867
dubhcait <dubhcait@live.ie>
2019-08-18 15:02:33 +0100

    add thought model

---
+++ src/database/models/thoughts.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ThoughtSchema = new Schema({
+  dateString: { type: String, required: true },
+  emoji: {
+    type: String,
+    required: true
+  },
+  thought: {
+    type: String,
+    required: true
+  }
+  // a reference to users collection
+  // publisher: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "users"
+  // }
+});
+
+module.exports = mongoose.model("thoughts", ThoughtSchema);
