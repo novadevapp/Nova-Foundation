0569680032dbad660805a384bb3c617bb07d9d29
dubhcait <dubhcait@live.ie>
2019-08-19 19:25:32 +0100

    add login handler

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const register = require('./signup');
-const pictures = require('./picture');
-const checkLogin = require('./checkLogin')
-const authenticate = require('../middlewares/authenticate');
-const login = require('./login');
+const register = require("./signup");
+const pictures = require("./picture");
+const checkLogin = require("./checkLogin");
+const authenticate = require("../middlewares/authenticate");
+const login = require("./login");
 
-router.use('/register', register);
-// router.use('/login', login);
+router.use("/register", register);
+router.use("/login", login);
 
 // Unlock Cookie Middleware
 router.use(authenticate);
-router.get('/login-status', checkLogin);
+router.get("/login-status", checkLogin);
 router.use(pictures);
 
 module.exports = router;
--- src/controller/login/postLogin.js
+++ src/controller/login/postLogin.js
@@ -1,1 +1,44 @@
+const hashPassword = require("../../utils/hash");
+const findUser = require("../../database/queries/selectUser");
+const { create } = require("../../utils/cookie");
 
+module.exports = async (req, res) => {
+  try {
+    const {
+      data: { email, password }
+    } = req.body;
+
+    // Check if email already exists
+    const user = await findUser({ email });
+    if (!user) throw Error("Email or password were incorrect");
+    console.log({ user });
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    if (!insertedUser._id === hashPassword)
+      throw Error("Email or password were incorrect");
+
+    // Create Cookie
+    const cookie = await create({
+      id: insertedUser._id,
+      username: insertedUser.username
+    });
+    // Set Cookie... maxAge => 2 months
+    res.cookie(
+      "jwt",
+      cookie,
+      { maxAge: 1000 * 3600 * 24 * 30 * 2 },
+      { HttpOnly: true }
+    );
+    return res.send({ data: { message: "Success" }, error: null });
+  } catch (error) {
+    // Error Cases
+    switch (error.message) {
+      case "Email or password were incorrect":
+        res.status(401).send({ data: null, error: error.message });
+        break;
+      default:
+        res.status(500).send({ data: null, error: "Internal Server Error" });
+    }
+  }
+};
