fd690618ba2b0921016f7b287677e9a19e8d8717
dubhcait <dubhcait@live.ie>
2019-08-19 20:06:15 +0100

    add notification to user for faile login attempt

--- client/src/Component/Pages/Login/index.js
+++ client/src/Component/Pages/Login/index.js
@@ -56,9 +56,19 @@ class Login extends Component {
       })
         .then(res => res.json())
         .then(data => {
-          return new Promise((resolve, reject) => {
-            resolve(this.props.setIsLogged(true));
-          }).then(() => this.props.history.push("/home"));
+          console.log(data);
+          if (data.data === null) {
+            notification(
+              this.notificationDOMRef,
+              "danger",
+              data.error,
+              "Error"
+            );
+          } else {
+            return new Promise((resolve, reject) => {
+              resolve(this.props.setIsLogged(true));
+            }).then(() => this.props.history.push("/home"));
+          }
         })
 
         .catch(() => {
@@ -76,28 +86,28 @@ class Login extends Component {
     return (
       <>
         <Header isLogged={false} />
-        <div className="login">
+        <div className='login'>
           <form onSubmit={this.handleSubmit}>
-            <h1 className="login__header">Please Login</h1>
+            <h1 className='login__header'>Please Login</h1>
             <Input
-              id="email"
-              label="Your email"
-              placeholder="Email"
-              type="email"
+              id='email'
+              label='Your email'
+              placeholder='Email'
+              type='email'
               action={this.handleInput}
             />
-            {email.error && <p className="login__error">{email.error}</p>}
+            {email.error && <p className='login__error'>{email.error}</p>}
             <Input
-              id="password"
-              label="Your password"
-              placeholder="Password"
-              type="password"
+              id='password'
+              label='Your password'
+              placeholder='Password'
+              type='password'
               action={this.handleInput}
             />
-            {password.error && <p className="login__error">{password.error}</p>}
+            {password.error && <p className='login__error'>{password.error}</p>}
             <Button
-              name="Login"
-              className="register__button login__btn"
+              name='Login'
+              className='register__button login__btn'
               onClick={this.handleSubmit}
             />
           </form>
