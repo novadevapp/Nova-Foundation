22e8a86b3f3454239c2f301e51be0a28669d3094
Israa <israa96sulaiman@gmail.com>
2019-08-20 20:55:16 +0300

    create post poem route

--- src/controller/poems/index.js
+++ src/controller/poems/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const getPoems = require("./getPoems");
+const postPoem = require('./postPoem');
 
 const route = express.Router();
 
-route.route('/').get(getPoems);
+route.route('/')
+.get(getPoems)
+.post(postPoem)
 
 module.exports = route;
