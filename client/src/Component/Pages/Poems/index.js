cfb73f84dfecbc6f64a65d23d9b804209ea9b334
Israa <israa96sulaiman@gmail.com>
2019-08-22 13:09:27 +0300

    edit indentation

--- client/src/Component/Pages/Poems/index.js
+++ client/src/Component/Pages/Poems/index.js
@@ -5,40 +5,40 @@ import Header from '../../CommonComponent/Header';
 import Footer from '../../CommonComponent/Footer';
 import Button from '../../CommonComponent/Button';
 
-export default  props => {
-	const [poems, setPoems] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState('');
+export default props => {
+  const [poems, setPoems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-	useEffect(() => 
-	{	fetch('/api/v1/poems')
-			.then(res => res.json())
-			.then(({ data, error }) => {
-				if (error) throw new Error('');
-				setPoems(data);
-				setLoading(true);
-			})
-			.catch(() => {
-				setError('Something Error Please Try Again');
-				setLoading(true);
-			})
-      return () => undefined;
-    }, []);
+  useEffect(() => {
+    fetch('/api/v1/poems')
+      .then(res => res.json())
+      .then(({ data, error }) => {
+        if (error) throw new Error('');
+        setPoems(data);
+        setLoading(true);
+      })
+      .catch(() => {
+        setError('Something Error Please Try Again');
+        setLoading(true);
+      })
+    return () => undefined;
+  }, []);
 
-	return (
-		<div>
-			<Header />
-			{loading && error && <p className="error">{error}</p>}
-			{loading && (
-				<>
-					<PoemList poems={poems} {...props} />
-					<div className="poem-buttons">
-						<Button className="large-back__button" name="Back" onClick={props.history.goBack} />
-						<Button className="register__button" name="More Poems" />
-					</div>
-				</>
-			)}
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      {loading && error && <p className="error">{error}</p>}
+      {loading && (
+        <>
+          <PoemList poems={poems} {...props} />
+          <div className="poem-buttons">
+            <Button className="large-back__button" name="Back" onClick={props.history.goBack} />
+            <Button className="register__button" name="More Poems" />
+          </div>
+        </>
+      )}
+      <Footer />
+    </div>
+  );
 };
