4cb1d5f73500770f15e05fa3fc05a7706979a46e
Israa <israa96sulaiman@gmail.com>
2019-08-22 12:32:47 +0300

    refactor poems page

--- client/src/Component/Pages/Poems/index.js
+++ client/src/Component/Pages/Poems/index.js
@@ -1,46 +1,44 @@
-import React, { useState, useEffect } from "react";
-import "./style.css";
-import PoemList from "./PoemList";
-import Header from "../../CommonComponent/Header";
-import Footer from "../../CommonComponent/Footer";
-import Button from "../../CommonComponent/Button";
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import PoemList from './PoemList';
+import Header from '../../CommonComponent/Header';
+import Footer from '../../CommonComponent/Footer';
+import Button from '../../CommonComponent/Button';
 
-const Poems = (props) => {
-  const [poems, setPoems] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+export default  props => {
+	const [poems, setPoems] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState('');
 
-  useEffect(() => {
-    if (!loading) {
-      fetch('/api/v1/poems')
-        .then(res => res.json())
-        .then(({ data, error }) => {
-          if (error) setError('Something Error Please Try Again')
-          else setPoems(data);
-          setLoading(true)
-        })
-        .catch(() => {
-          setError('Something Error Please Try Again')
-        })
-    }
-  });
-  return (
-    <div>
-      <Header />
-      {error && <p className="error">{error}</p>}
-      {loading ?
-        <>
-          <PoemList poems={poems} {...props} />
-          <div className="poem-buttons">
-            <Button className="large-back__button" name="Back" onClick={() => props.history.goBack()} />
-            <Button className="register__button" name="More Poems" />
-          </div>
-        </>
-        : null}
-      <Footer />
-    </div>
+	useEffect(() => 
+	{	fetch('/api/v1/poems')
+			.then(res => res.json())
+			.then(({ data, error }) => {
+				if (error) throw new Error('');
+				setPoems(data);
+				setLoading(true);
+			})
+			.catch(() => {
+				setError('Something Error Please Try Again');
+				setLoading(true);
+			})
+      return () => undefined;
+    }, []);
 
-  );
+	return (
+		<div>
+			<Header />
+			{loading && error && <p className="error">{error}</p>}
+			{loading && (
+				<>
+					<PoemList poems={poems} {...props} />
+					<div className="poem-buttons">
+						<Button className="large-back__button" name="Back" onClick={props.history.goBack} />
+						<Button className="register__button" name="More Poems" />
+					</div>
+				</>
+			)}
+			<Footer />
+		</div>
+	);
 };
-
-export default Poems;
