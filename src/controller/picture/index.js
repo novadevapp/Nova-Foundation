bf29001b3044639090976c4daac905dc7bed501d
dubhcait <dubhcait@live.ie>
2019-08-30 22:24:54 +0100

    got image into cloudinary account

--- src/controller/picture/index.js
+++ src/controller/picture/index.js
@@ -3,14 +3,26 @@ const express = require("express");
 const pictures = require("./getPictures");
 const postPicture = require("./postPicture");
 const deletePicture = require("./deletePicture");
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
 
 const route = express.Router();
+
+cloudinary.config();
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "demo",
+  allowedFormats: ["jpg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+});
+const parser = multer({ storage: storage });
 
 route
   .route("/pictures")
   .get(pictures)
   .delete(deletePicture);
-  
-route.post("/add-picture", postPicture);
+
+route.post("/add-picture", parser.single("file"), postPicture);
 
 module.exports = route;
--- src/controller/picture/postPicture.js
+++ src/controller/picture/postPicture.js
@@ -1,3 +1,20 @@
-const postPicture = async (req, res) => {};
+const { config, uploader } = require("cloudinary").v2;
+const path = require("path");
+
+const postPicture = async (req, res) => {
+  console.log(req.file);
+  const { title } = req.body;
+  let { id } = req.auth;
+  file.name = `${Date.now()}${path.extname(file.name)}`;
+
+  const image = {};
+  image.url = req.file.url;
+  image.id = req.file.public_id;
+
+  console.log({ image });
+  // Image.create(image) // save image information in database
+  // .then(newImage => res.json(newImage))
+  //.catch(err => console.log(err));
+};
 
 module.exports = postPicture;
